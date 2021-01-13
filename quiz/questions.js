7646cd79faa6886e1d2612bd126f3736466f8934
sliappi <72688949+sliappi@users.noreply.github.com>
2021-01-13 11:48:56 +0200

    Add files via upload

---
+++ quiz/questions.js
@@ -0,0 +1,222 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What type of galaxy is the most common in the universe?",
+    answer: "Elliptical galaxies",
+    options: [
+      "Elliptical galaxies",
+      "Spiral galaxies",
+      "Regular galaxies",
+      "Irregular galaxies"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What percent of the universe is dark matter?",
+    answer: "27%",
+    options: [
+      "17%",
+      "27%",
+      "37%",
+      "47%"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the most common type of star found in the Milky Way?",
+    answer: "Red dwarf stars",
+    options: [
+      "Neutron stars",
+      "Red dwarf stars",
+      "Main Sequence stars",
+      "Supergiant stars"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the longest continuous time a human has spent in space?",
+    answer: "437 days",
+    options: [
+      "347 days",
+      "374 days",
+      "437 days",
+      "473 days"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the diameter of the Earth's moon?",
+    answer: "2,159 miles",
+    options: [
+      "2,159 miles",
+      "1,592 miles",
+      "5,921 miles",
+      "9,215 miles"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What is the coldest place in the universe?",
+    answer: "The Boomerang Nebula",
+    options: [
+        "The Dark Side of the Moon",
+        "The Boomerang Nebula",
+        "The Cold Atom Lab",
+        "Pluto"
+    ]
+   },
+    {
+    numb: 7,
+    question: "How many planets are in the Solar System?",
+    answer: "Eight planets",
+    options: [
+        "Seven planets",
+        "Eight planets",
+        "Nine planets",
+        "Ten planets"
+    ]
+   },
+   {
+    numb: 8,
+    question: "What is the largest type of star in the universe?",
+    answer: "Red supergiant stars",
+    options: [
+        "Red supergiant stars",
+        "Sun",
+        "T Tauri stars",
+        "Main Sequence stars"
+    ]
+   },
+   {
+    numb: 9,
+    question: "What is the farthest distance from Earth a manned mission has traveled?",
+    answer: "248,655 miles",
+    options: [
+        "246,855 miles",
+        "248,655 miles",
+        "264,855 miles",
+        "268,455 miles"
+    ]
+   },
+   {
+    numb: 10,
+    question: "What is the largest crater on Earth's moon?",
+    answer: "South Pole - Aitken basin",
+    options: [
+        "Mare Imbrium",
+        "Moltke",
+        "South Pole - Aitken basin",
+        "Tycho"
+    ]
+   },
+   {
+    numb: 11,
+    question: "The hottest place in the universe is located in which constellation?",
+    answer: "The constellation Virgo",
+    options: [
+        "The constellation Virgo",
+        "The constellation Orion",
+        "The constellation Scorpius",
+        "The constellation Ursa Major"
+    ]
+   },
+   {
+    numb: 12,
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter",
+    options: [
+        "Earth",
+        "Venus",
+        "Jupiter",
+        "Mars"
+    ]
+   },
+   {
+    numb: 13,
+    question: "What is the closest star to the Sun?",
+    answer: "Proxima Centauri",
+    options: [
+        "Proxima Centauri",
+        "Barnard's Star",
+        "Lalande",
+        "Luyten's Star"
+    ]
+   },
+   {
+    numb: 14,
+    question: "How many minutes was the shortest space flight?",
+    answer: "15 minutes",
+    options: [
+        "45 minutes",
+        "35 minutes",
+        "25 minutes",
+        "15 minutes"
+    ]
+   },
+   {
+    numb: 15,
+    question: "What are the dimensions in inches of the first footprint on Earth's moon?",
+    answer: "13 by 6 inches",
+    options: [
+        "13 by 6 inches",
+        "13 by 5 inches",
+        "13 by 4 inches",
+        "13 by 3 inches"
+    ]
+   },
+   {
+    numb: 16,
+    question: "How old is the universe in years?",
+    answer: "13,8 billion years old",
+    options: [
+        "31,8 billion years old",
+        "18,3 billion yearws old",
+        "38,1 billion years old",
+        "13,8 billion years old"
+    ]
+   },
+   {
+    numb: 17,
+    question: "What is the smallest planet in our solar system?",
+    answer: "Mercury",
+    options: [
+        "Pluto",
+        "Mars",
+        "Earth",
+        "Mercury"
+    ]
+   },
+   {
+    numb: 18,
+    question: "Which NASA space flight was the last manned mission to the moon?",
+    answer: "Apollo 17",
+    options: [
+        "Apollo 1",
+        "Apollo 11",
+        "Apollo 13",
+        "Apollo 17"
+    ]
+   },
+   {
+    numb: 19,
+    question: "How many moons are in our Solar System?",
+    answer: "181 moons",
+    options: [
+        "151 moons",
+        "161 moons",
+        "171 moons",
+        "181 moons"
+    ]
+   },
+   {
+    numb: 20,
+    question: "What flavor ice cream did Baskin-Robbins release in 1969 to commemorate America's landing on the moon?",
+    answer: "Lunar Cheesecake",
+    options: [
+        "Lunar Biscuit",
+        "Lunar Apple Pie",
+        "Lunar Cheesecake",
+        "Lunar Chocolate"
+    ]
+   },
+];
